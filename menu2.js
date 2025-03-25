efce1d61b34ef9a8c6aac2692c85bc9b0e88f00d
gintingprojects19 <162718453+gintingprojects19@users.noreply.github.com>
2025-03-25 21:58:19 +0700

    Add files via upload

---
+++ menu2.js
@@ -0,0 +1,28 @@
+const menu = document.getElementById("menu");
+const openMenu = document.getElementById("list-menu");
+menu.textContent="Menu";
+menu.addEventListener("click", event =>{
+
+    if(openMenu.style.display==="none"){
+       openMenu.style.display="block";
+        menu.textContent="Close";
+       }else{
+       openMenu.style.display="none";
+        menu.textContent="Menu";
+       }
+/*    menu.textContent="Close";
+    openMenu.style.display="none";
+   
+    menu.style.color="white";
+    menu.style.textAlign="center";
+    menu.style.fontSize="16pt";*/
+
+})
+/*openMenu.style.display="none";
+menu.textContent="Show";
+menu.style.color="white";
+menu.style.textAlign="center";
+menu.style.fontSize="16pt";*/
+cls.addEventListener("click", event=>{
+    openMenu.style.display="none";
+})
